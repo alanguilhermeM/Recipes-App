98f69a93beb63f018134a97c5c04ba6ba4bdc26d
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2023-06-06 14:14:44 -0300

    arquivo de teste do footer

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
+import Provider from '../context/myProvider';
 import App from '../App';
 
-const renderWithRouter = (component) => {
-  const history = createMemoryHistory();
+const renderWithRouterAndContext = (component, path = '/') => {
+  const history = createMemoryHistory({ initialEntries: [path] });
   return ({
-    ...render(<Router history={ history }>{component}</Router>), history,
+    ...render(
+      <Provider>
+        <Router history={ history }>
+          {component}
+        </Router>
+      </Provider>,
+    ),
+    history,
   });
 };
 
 describe('Testes para o componente Footer', () => {
-  it('', () => {
-    renderWithRouter(<App />);
+  it('Teste se renderiza um footer na page meals', () => {
+    const { history } = renderWithRouterAndContext(<App />, '/meals');
+    expect(history.location.pathname).toBe('/meals');
   });
 });

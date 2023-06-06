0ea3a8dba6f1baf06f171f7fadb5da99d7eeae9d
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2023-06-06 14:46:42 -0300

    testestes para footer

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import Provider from '../context/myProvider';
 import App from '../App';
 
@@ -20,8 +20,30 @@ const renderWithRouterAndContext = (component, path = '/') => {
 };
 
 describe('Testes para o componente Footer', () => {
-  it('Teste se renderiza um footer na page meals', () => {
+  const btnMeals = screen.getByTestId('meals-bottom-btn');
+  const btnDrinks = screen.getByTestId('drinks-bottom-btn');
+
+  it('Teste se renderiza um footer na page meals, com dois buttons', () => {
     const { history } = renderWithRouterAndContext(<App />, '/meals');
     expect(history.location.pathname).toBe('/meals');
+
+    expect(btnDrinks).toBeDefined();
+    expect(btnMeals).toBeDefined();
+  });
+
+  it('Teste se renderiza um footer na page drinks, com dois buttons', () => {
+    const { history } = renderWithRouterAndContext(<App />, '/drinks');
+    expect(history.location.pathname).toBe('/drinks');
+
+    expect(btnDrinks).toBeDefined();
+    expect(btnMeals).toBeDefined();
+  });
+
+  it('Teste se renderiza um footer na page profile, com dois buttons', () => {
+    const { history } = renderWithRouterAndContext(<App />, '/profile');
+    expect(history.location.pathname).toBe('/profile');
+
+    expect(btnDrinks).toBeDefined();
+    expect(btnMeals).toBeDefined();
   });
 });

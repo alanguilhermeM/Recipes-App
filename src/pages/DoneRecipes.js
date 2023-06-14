0a2ec8a38ca3916b3c2cfd42e70e7a8252b7ed27
luisnaoki <luis_naoki@hotmail.com>
2023-06-14 17:02:17 -0400

    Requisito 44- 49

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,12 +1,137 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import shareIcon from '../images/shareIcon.svg';
 
-export default function DoneRecipes() {
+function DoneRecipes() {
+  const [doneRecipes, setDoneRecipes] = useState([]);
+  const [copied, setCopied] = useState(false);
+  const [filter, setFilter] = useState('All');
+
+  useEffect(() => {
+    const getDones = JSON.parse(localStorage.getItem('doneRecipes', []));
+    setDoneRecipes(getDones);
+  }, []);
+
+  const handleButton = (type, id) => {
+    setCopied(true);
+    navigator.clipboard.writeText(`${window.location.origin}/${type}s/${id}`);
+  };
   return (
+
     <div>
       <Header />
-      DoneRecipes
 
+      <div>
+        <button
+          data-testid="filter-by-drink-btn"
+          type="button"
+          onClick={ () => setFilter('drinks') }
+        >
+          Drinks
+        </button>
+
+        <button
+          data-testid="filter-by-meal-btn"
+          type="button"
+          onClick={ () => setFilter('meals') }
+        >
+          Meals
+        </button>
+
+        <button
+          data-testid="filter-by-all-btn"
+          type="button"
+          onClick={ () => setFilter('all') }
+        >
+          All
+        </button>
+      </div>
+
+      <div>
+        {
+          doneRecipes.filter((element) => {
+            switch (filter) {
+            case 'meals': return element.type === 'meal';
+            case 'drinks': return element.type === 'drink';
+            default: return element;
+            }
+          })
+            .map((e, index) => (
+              <div
+                key={ index }
+              >
+                <Link to={ `/${e.type}s/${e.id}` }>
+                  <img
+                    alt={ e.id }
+                    src={ e.image }
+                    width="200px"
+                    data-testid={ `${index}-horizontal-image` }
+                  />
+                </Link>
+
+                <Link to={ `/${e.type}s/${e.id}` }>
+                  <div
+                    data-testid={ `${index}-horizontal-name` }
+                  >
+                    {e.name}
+                  </div>
+                </Link>
+
+                {
+                  e.type === 'meal' ? (
+                    <p
+                      data-testid={ `${index}-horizontal-top-text` }
+                    >
+                      {`${e.nationality} - ${e.category}`}
+                    </p>)
+                    : (
+                      <p
+                        data-testid={ `${index}-horizontal-top-text` }
+                      >
+                        {e.alcoholicOrNot}
+                      </p>)
+                }
+                <p
+                  data-testid={ `${index}-horizontal-done-date` }
+                >
+                  {e.doneDate}
+
+                </p>
+
+                {
+                  copied && <p data-testid="copied-msg">Link copied!</p>
+                }
+                <div>
+                  {
+                    e.tags.map((tag) => (
+                      <div
+                        data-testid={ `${index}-${tag}-horizontal-tag` }
+                        key={ tag }
+                      >
+                        #
+                        {tag}
+                      </div>
+                    ))
+                  }
+                </div>
+                <button
+                  data-testid="share-btn"
+                  type="button"
+                  onClick={ () => handleButton(e.type, e.id) }
+                >
+                  <img
+                    data-testid={ `${index}-horizontal-share-btn` }
+                    alt="Share Icon"
+                    src={ shareIcon }
+                  />
+                </button>
+              </div>
+            ))
+        }
+      </div>
     </div>
   );
 }
+
+export default DoneRecipes;
---
+++ src/tests/DoneRecipes.test.js
@@ -0,0 +1,49 @@
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import React from 'react';
+import { Router } from 'react-router-dom';
+import App from '../App';
+import Provider from '../context/myProvider';
+
+describe('Tests for "DoneRecipes" page', () => {
+  beforeEach(() => {
+    const history = createMemoryHistory();
+    history.push('/done-recipes');
+    render(
+      <Router history={ history }>
+        <Provider>
+          <App />
+        </Provider>
+      </Router>,
+    );
+  });
+
+  it('Tests the filter buttons', () => {
+    const filters = ['All', 'Meal', 'Drink'];
+    filters.forEach((filter) => {
+      const filterButton = screen.getByText(filter);
+      expect(filterButton).toBeInTheDocument();
+    });
+  });
+
+  it('Tests the rendering of recipes', () => {
+    mockDoneRecipes.forEach((recipe) => {
+      const recipeItem = screen.getByText(recipe.strMeal || recipe.strDrink);
+      expect(recipeItem).toBeInTheDocument();
+    });
+  });
+
+  it('Tests the filtering of recipes', () => {
+    const mealsButton = screen.getByText(/meal/i);
+    act(() => {
+      userEvent.click(mealsButton);
+    });
+
+    const drinkRecipe = screen.queryByText(/aquamarine/i);
+    expect(drinkRecipe).not.toBeInTheDocument();
+
+    const mealRecipe = screen.getByText(/spicy/i);
+    expect(mealRecipe).toBeInTheDocument();
+  });
+});
